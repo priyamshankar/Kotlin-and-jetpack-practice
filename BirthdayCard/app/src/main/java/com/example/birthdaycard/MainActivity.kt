4e284bf9079c7cefa70a932c8847cc96a4951893
Shankar <pshanka4@visteon.com>
2024-01-31 16:48:08 +0530

    background picture but with bad vertical allignment

--- BirthdayCard/app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ BirthdayCard/app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -15,10 +15,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.birthdaycard.ui.theme.BirthdayCardTheme
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 
 
 class MainActivity : ComponentActivity() {
@@ -43,15 +46,31 @@ class MainActivity : ComponentActivity() {
 fun BirthdayCard (name:String, recepient : String){
     BirthdayCardTheme{
         Surface{
-            Column(verticalArrangement = Arrangement.Center,
-                modifier = Modifier.padding(all = 10.dp)) {
-                Text (text="Demo text to showcase ",fontSize = 36.sp, lineHeight = 50.sp)
-                Text (text = "$name!",
-                    modifier = Modifier.align(alignment =
-                    Alignment.CenterHorizontally),
-                    lineHeight = 130.sp, fontSize = 36.sp)
-                Spacer(modifier = Modifier.height(9.dp))
-                Text (text = "- From $recepient", modifier = Modifier.align(alignment = Alignment.End))
+            Box {
+                val img = painterResource(id = R.drawable.androidparty)
+                Image(
+                    painter = img,//or we can use painter = painterResource (id=R.drawable.androidparty)
+                    contentDescription = null
+                )
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.padding(all = 10.dp)
+                ) {
+                    Text(text = "Demo text to showcase ", fontSize = 36.sp, lineHeight = 50.sp)
+                    Text(
+                        text = "$name!",
+                        modifier = Modifier.align(
+                            alignment =
+                            Alignment.CenterHorizontally
+                        ),
+                        lineHeight = 130.sp, fontSize = 36.sp
+                    )
+                    Spacer(modifier = Modifier.height(9.dp))
+                    Text(
+                        text = "- From $recepient",
+                        modifier = Modifier.align(alignment = Alignment.End)
+                    )
+                }
             }
         }
     }
