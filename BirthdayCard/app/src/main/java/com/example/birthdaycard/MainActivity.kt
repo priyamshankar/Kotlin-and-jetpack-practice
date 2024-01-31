5e6b6877e0c44716a631d4f68fdd1806c49aebcf
Shankar <pshanka4@visteon.com>
2024-01-31 14:44:10 +0530

    birthday card basic layout

--- BirthdayCard/app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ BirthdayCard/app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.birthdaycard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.birthdaycard.ui.theme.BirthdayCardTheme
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -40,9 +43,13 @@ class MainActivity : ComponentActivity() {
 fun BirthdayCard (name:String, recepient : String){
     BirthdayCardTheme{
         Surface{
-            Column {
-                Text (text="Happy Brthday ")
-                Text (text = "$name!", modifier = Modifier.align(alignment = Alignment.End))
+            Column(verticalArrangement = Arrangement.Center,
+                modifier = Modifier.padding(all = 10.dp)) {
+                Text (text="Demo text to showcase ",fontSize = 36.sp, lineHeight = 50.sp)
+                Text (text = "$name!",
+                    modifier = Modifier.align(alignment =
+                    Alignment.CenterHorizontally),
+                    lineHeight = 130.sp, fontSize = 36.sp)
                 Spacer(modifier = Modifier.height(9.dp))
                 Text (text = "- From $recepient", modifier = Modifier.align(alignment = Alignment.End))
             }
