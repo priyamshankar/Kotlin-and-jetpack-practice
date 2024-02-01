c789f55b08cd9ca03607ba926be9f3e46d81288c
Shankar <pshanka4@visteon.com>
2024-02-01 10:31:55 +0530

    alignment done

--- AlignmentTutorial/app/src/main/java/com/example/alignmenttutorial/MainActivity.kt
+++ AlignmentTutorial/app/src/main/java/com/example/alignmenttutorial/MainActivity.kt
@@ -3,15 +3,22 @@ package com.example.alignmenttutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.alignmenttutorial.ui.theme.AlignmentTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,15 +41,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AlignFn(textToWrite: String) {
     AlignmentTutorialTheme {
-        Surface {
-            Row {
-                Column {
+        Surface (
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row(modifier = Modifier.background(color=Color.Gray).padding(15.dp).background(color= Color.Yellow).padding(19.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically) {
+                Column (modifier = Modifier.background(color = Color.White).padding(8.dp)) {
 
                     for (x in 1..5) {
                         Text(text = "$x : $textToWrite")
                     }
                 }
-                Text(text = "hello $textToWrite", modifier = Modifier.fillMaxSize())
+                Text(text = "hello $textToWrite", modifier = Modifier.background(color = Color.Gray))
             }
         }
     }
