659d7b4da9ea8a849ce8794f81555ab3718a9a89
Shankar <pshanka4@visteon.com>
2024-03-08 10:48:40 +0530

    name page ui done

---
+++ JlrSignin/app/src/main/java/com/example/jlrsignin/presentation/view/EnterNamePage.kt
@@ -0,0 +1,61 @@
+package com.example.jlrsignin.presentation.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun EnterNameComposable(modifier: Modifier) {
+    var nameVar by remember {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+            .wrapContentSize(Alignment.Center),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Enter Your Name",
+            fontWeight = FontWeight.Bold,
+            fontSize = 32.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            textAlign = TextAlign.Center
+        )
+        TextField(value = nameVar, onValueChange = { nameVar = it })
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = modifier.padding(5.dp)
+        ) {
+            Text(text = "Next")
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Enternameprev() {
+    EnterNameComposable(modifier = Modifier)
+}
